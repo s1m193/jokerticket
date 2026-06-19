e25d6ecbfd957cb67b2d5547d90251be31d464da
hazem884 <h71498362@gmail.com>
2026-06-19 05:55:58 +0300

    Update Silver-Ticket.py

--- jokerticket/kerberos/Silver-Ticket.py
+++ jokerticket/kerberos/Silver-Ticket.py
@@ -1693,7 +1693,7 @@ if __name__ == '__main__':
 
     dc_ip = get_input(
         Fore.CYAN + "[?] Enter DC IP Address          : " + Style.RESET_ALL,
-        validate_ip, "Invalid IP! Example: 192.168.1.1"
+        validate_ip, "Invalid IP! Example: 192.168.x.x"
     )
 
     print(Fore.YELLOW + "[*] Checking DC reachability..." + Style.RESET_ALL)
@@ -1703,7 +1703,7 @@ if __name__ == '__main__':
     print(Fore.GREEN + f"[+] DC {dc_ip} is reachable!" + Style.RESET_ALL)
 
     domain   = get_input(Fore.CYAN + "[?] Enter Domain Name            : " + Style.RESET_ALL,
-                         validate_domain, "Invalid domain! Example: cs.org")
+                         validate_domain, "Invalid domain! Example: domain.com")
     username = get_input(Fore.CYAN + "[?] Enter Username               : " + Style.RESET_ALL)
     password = get_input(Fore.CYAN + "[?] Enter Password               : " + Style.RESET_ALL)
 
@@ -1742,7 +1742,7 @@ if __name__ == '__main__':
 
     dc_hostname = get_input(
         Fore.CYAN   + "[?] Enter DC Hostname (FQDN)     : " + Style.RESET_ALL +
-        Fore.YELLOW + f"(default: {dc_hostname_auto}) " if dc_hostname_auto else Fore.YELLOW + "(e.g. DC01.cs.org) " + Style.RESET_ALL,
+        Fore.YELLOW + f"(default: {dc_hostname_auto}) " if dc_hostname_auto else Fore.YELLOW + "(e.g. DC01.domain.com) " + Style.RESET_ALL,
         allow_empty=True
     ) or dc_hostname_auto
 
